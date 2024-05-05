ac259c659f163c1d7f5763dac771f9ed66e27896
Pablo Castillo <pablocastillo.5.1992@gmail.com>
2024-05-05 16:42:25 -0500

    Proyecto inicializado con un TestController

---
+++ src/main/java/com/pdev/DemoAppForRailway/DemoAppForRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.pdev.DemoAppForRailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppForRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppForRailwayApplication.class, args);
+	}
+
+}
